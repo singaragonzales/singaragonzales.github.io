584f6cdfa2df7828334e646af492ce14284f393b
SingaraSDS <singara@sdsharp.com>
2022-12-05 02:19:47 -0500

    finish multilanguage and update performance part 2

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,3 +1,4 @@
+import { t } from 'i18next'
 import { Fox } from '../../assets/image'
 import "./contact.scss"
 
@@ -7,10 +8,10 @@ const Contact = () => {
         <section className="contact__container">
             <div className="contact section contact__box  wrapper  pad-before" id="contact">
                 <div className='max-w-[500px]'>
-                    <h2 className="contact__title">Hey there!</h2>
-                    <p className="contact__subtitle">If you want to contact me you can do it through this email {" "}
-                        <a href='mailto:singara.gonzales@gmail.com' rel="noreferrer" target="_blank" className='underline' aria-label="Send me an email">singara.gonzales@gmail.com</a>, 
-                        or send me a message on <a href='#home' className='underline' aria-label="Go to my social networks buttons">social networks</a>.
+                    <h2 className="contact__title">{t("hey.there")}</h2>
+                    <p className="contact__subtitle">{t("contact.final.message.1")}
+                        <a href='mailto:singara.gonzales@gmail.com' rel="noreferrer" target="_blank" className='underline' aria-label="Send me an email">{t("email")}</a>, 
+                        {t("contact.final.message.2")}<a href='#home' className='underline' aria-label="Go to my social networks buttons">{t("social")}</a>.
                     </p>
                 </div>
                 <div className="generator__shape">
